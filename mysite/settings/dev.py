c2c0532058eb3e4b5b2a63c8f37abdf04b196f00
Wallace Caetano <wallace.santos@youtan.com.br>
2026-02-24 13:01:05 -0300

    base project commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*p^y%o_#4%@o=*t7#q01iufy*ks_vjm7=b0f_ln1=ui$k=th+="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
